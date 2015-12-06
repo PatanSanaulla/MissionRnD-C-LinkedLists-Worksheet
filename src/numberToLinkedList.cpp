512aa9e958a5faf3aa791b82d7f373c7e9bda8e4
Sanaulla khan <spatankhan@gmail.com>
2015-12-06 14:11:17 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -20,5 +20,34 @@ struct node {
 };
 
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	struct node *p, *first;
+	p = NULL;
+	first = NULL;
+	if (N < 0){ N = 0-N; }
+	int digit;
+	if (N == 0){
+		first = (struct node*)malloc(sizeof(struct node*));
+		first->num = 0;
+		first->next = NULL;
+	}
+	else{
+		while (N > 0)
+		{
+			digit = N % 10;
+			N = N / 10;
+			if (first == NULL){
+				first = (struct node*)malloc(sizeof(struct node*));
+				first->num = digit;
+				first->next = NULL;
+			}
+			else{
+				p = (struct node*)malloc(sizeof(struct node*));
+				p->next = first;
+				p->num = digit;
+				first = p;
+			}
+
+		}
+	}
+	return first;
 }
